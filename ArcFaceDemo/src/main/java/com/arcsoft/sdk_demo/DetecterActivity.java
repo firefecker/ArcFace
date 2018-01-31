0831c3b81f192a671e6aa74ab66871063b73e378
firecoding <1606561454@qq.com>
2018-01-31 14:56:50 +0800

    update

--- ArcFaceDemo/src/main/java/com/arcsoft/sdk_demo/DetecterActivity.java
+++ ArcFaceDemo/src/main/java/com/arcsoft/sdk_demo/DetecterActivity.java
@@ -16,11 +16,6 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-//import com.arcsoft.ageestimation.ASAE_FSDKAge;
-//import com.arcsoft.ageestimation.ASAE_FSDKEngine;
-//import com.arcsoft.ageestimation.ASAE_FSDKError;
-//import com.arcsoft.ageestimation.ASAE_FSDKFace;
-//import com.arcsoft.ageestimation.ASAE_FSDKVersion;
 import com.arcsoft.facerecognition.AFR_FSDKEngine;
 import com.arcsoft.facerecognition.AFR_FSDKError;
 import com.arcsoft.facerecognition.AFR_FSDKFace;
@@ -30,11 +25,6 @@ import com.arcsoft.facetracking.AFT_FSDKEngine;
 import com.arcsoft.facetracking.AFT_FSDKError;
 import com.arcsoft.facetracking.AFT_FSDKFace;
 import com.arcsoft.facetracking.AFT_FSDKVersion;
-//import com.arcsoft.genderestimation.ASGE_FSDKEngine;
-//import com.arcsoft.genderestimation.ASGE_FSDKError;
-//import com.arcsoft.genderestimation.ASGE_FSDKFace;
-//import com.arcsoft.genderestimation.ASGE_FSDKGender;
-//import com.arcsoft.genderestimation.ASGE_FSDKVersion;
 import com.guo.android_extend.java.AbsLoop;
 import com.guo.android_extend.java.ExtByteArrayOutputStream;
 import com.guo.android_extend.tools.CameraHelper;
@@ -64,13 +54,8 @@ public class DetecterActivity extends Activity implements OnCameraListener, View
 
 	AFT_FSDKVersion version = new AFT_FSDKVersion();
 	AFT_FSDKEngine engine = new AFT_FSDKEngine();
-	//ASAE_FSDKVersion mAgeVersion = new ASAE_FSDKVersion();
-	//ASAE_FSDKEngine mAgeEngine = new ASAE_FSDKEngine();
-	//ASGE_FSDKVersion mGenderVersion = new ASGE_FSDKVersion();
-	//ASGE_FSDKEngine mGenderEngine = new ASGE_FSDKEngine();
+
 	List<AFT_FSDKFace> result = new ArrayList<>();
-	//List<ASAE_FSDKAge> ages = new ArrayList<>();
-	//List<ASGE_FSDKGender> genders = new ArrayList<>();
 
 	int mCameraID;
 	int mCameraRotate;
@@ -94,15 +79,14 @@ public class DetecterActivity extends Activity implements OnCameraListener, View
 		AFR_FSDKEngine engine = new AFR_FSDKEngine();
 		AFR_FSDKFace result = new AFR_FSDKFace();
 		List<FaceDB.FaceRegist> mResgist = ((Application)DetecterActivity.this.getApplicationContext()).mFaceDB.mRegister;
-		//List<ASAE_FSDKFace> face1 = new ArrayList<>();
-		//List<ASGE_FSDKFace> face2 = new ArrayList<>();
 		
 		@Override
 		public void setup() {
 			AFR_FSDKError error = engine.AFR_FSDK_InitialEngine(FaceDB.appid, FaceDB.fr_key);
 			Log.d(TAG, "AFR_FSDK_InitialEngine = " + error.getCode());
 			error = engine.AFR_FSDK_GetVersion(version);
-			Log.d(TAG, "FR=" + version.toString() + "," + error.getCode()); //(210, 178 - 478, 446), degree = 1　780, 2208 - 1942, 3370
+			Log.d(TAG, "FR=" + version.toString() + "," + error.getCode());
+			//(210, 178 - 478, 446), degree = 1　780, 2208 - 1942, 3370
 		}
 
 		@Override
@@ -125,18 +109,6 @@ public class DetecterActivity extends Activity implements OnCameraListener, View
 						}
 					}
 				}
-
-				//age & gender
-				//face1.clear();
-				//face2.clear();
-				//face1.add(new ASAE_FSDKFace(mAFT_FSDKFace.getRect(), mAFT_FSDKFace.getDegree()));
-				//face2.add(new ASGE_FSDKFace(mAFT_FSDKFace.getRect(), mAFT_FSDKFace.getDegree()));
-				//ASAE_FSDKError error1 = mAgeEngine.ASAE_FSDK_AgeEstimation_Image(mImageNV21, mWidth, mHeight, AFT_FSDKEngine.CP_PAF_NV21, face1, ages);
-				//ASGE_FSDKError error2 = mGenderEngine.ASGE_FSDK_GenderEstimation_Image(mImageNV21, mWidth, mHeight, AFT_FSDKEngine.CP_PAF_NV21, face2, genders);
-				//Log.d(TAG, "ASAE_FSDK_AgeEstimation_Image:" + error1.getCode() + ",ASGE_FSDK_GenderEstimation_Image:" + error2.getCode());
-				//Log.d(TAG, "age:" + ages.get(0).getAge() + ",gender:" + genders.get(0).getGender());
-				//final String age = ages.get(0).getAge() == 0 ? "年龄未知" : ages.get(0).getAge() + "岁";
-				//final String gender = genders.get(0).getGender() == -1 ? "性别未知" : (genders.get(0).getGender() == 0 ? "男" : "女");
 				
 				//crop
 				byte[] data = mImageNV21;
@@ -181,7 +153,6 @@ public class DetecterActivity extends Activity implements OnCameraListener, View
 						public void run() {
 							mTextView.setAlpha(1.0f);
 							mTextView1.setVisibility(View.VISIBLE);
-							//mTextView1.setText( gender + "," + age);
 							mTextView1.setTextColor(Color.RED);
 							mTextView.setText(mNameShow);
 							mTextView.setTextColor(Color.RED);
@@ -247,16 +218,6 @@ public class DetecterActivity extends Activity implements OnCameraListener, View
 		err = engine.AFT_FSDK_GetVersion(version);
 		Log.d(TAG, "AFT_FSDK_GetVersion:" + version.toString() + "," + err.getCode());
 
-		//ASAE_FSDKError error = mAgeEngine.ASAE_FSDK_InitAgeEngine(FaceDB.appid, FaceDB.age_key);
-		//Log.d(TAG, "ASAE_FSDK_InitAgeEngine =" + error.getCode());
-		//error = mAgeEngine.ASAE_FSDK_GetVersion(mAgeVersion);
-		//Log.d(TAG, "ASAE_FSDK_GetVersion:" + mAgeVersion.toString() + "," + error.getCode());
-
-		//ASGE_FSDKError error1 = mGenderEngine.ASGE_FSDK_InitgGenderEngine(FaceDB.appid, FaceDB.gender_key);
-		//Log.d(TAG, "ASGE_FSDK_InitgGenderEngine =" + error1.getCode());
-		//error1 = mGenderEngine.ASGE_FSDK_GetVersion(mGenderVersion);
-		//Log.d(TAG, "ASGE_FSDK_GetVersion:" + mGenderVersion.toString() + "," + error1.getCode());
-
 		mFRAbsLoop = new FRAbsLoop();
 		mFRAbsLoop.start();
 	}
@@ -272,11 +233,6 @@ public class DetecterActivity extends Activity implements OnCameraListener, View
 		AFT_FSDKError err = engine.AFT_FSDK_UninitialFaceEngine();
 		Log.d(TAG, "AFT_FSDK_UninitialFaceEngine =" + err.getCode());
 
-		//ASAE_FSDKError err1 = mAgeEngine.ASAE_FSDK_UninitAgeEngine();
-		//Log.d(TAG, "ASAE_FSDK_UninitAgeEngine =" + err1.getCode());
-
-		//ASGE_FSDKError err2 = mGenderEngine.ASGE_FSDK_UninitGenderEngine();
-		//Log.d(TAG, "ASGE_FSDK_UninitGenderEngine =" + err2.getCode());
 	}
 
 	@Override
@@ -302,13 +258,6 @@ public class DetecterActivity extends Activity implements OnCameraListener, View
 					Log.d(TAG, "V=" + data);
 				}
 			}
-			//parameters.setPreviewFpsRange(15000, 30000);
-			//parameters.setExposureCompensation(parameters.getMaxExposureCompensation());
-			//parameters.setWhiteBalance(Camera.Parameters.WHITE_BALANCE_AUTO);
-			//parameters.setAntibanding(Camera.Parameters.ANTIBANDING_AUTO);
-			//parmeters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
-			//parameters.setSceneMode(Camera.Parameters.SCENE_MODE_AUTO);
-			//parameters.setColorEffect(Camera.Parameters.EFFECT_NONE);
 			mCamera.setParameters(parameters);
 		} catch (Exception e) {
 			e.printStackTrace();
